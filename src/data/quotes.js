4a8dfaf946cd29052597be94c23708951f10939d
Souhail-Bel <robotboy976@gmail.com>
2026-05-03 17:41:50 +0100

    Added quotes and GIFs

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -3,4 +3,9 @@ export const quotesList = [
   "화이팅!\nDo your best!",
   "고생 끝에 낙이 온다\nAt the end of hardship comes happiness",
   "한 우물을 파라\nDig one well (not many different wells)",
+  "로마는 하루아침에 이루어진 것이 아니다\nRome wasn’t made in one morning",
+  "뜻이 있는 곳에 길이 있다\nWhere there’s a will, there’s a way",
+  "궁하면 통한다\nThere is always a way out",
+  "피할 수 없으면 즐겨라\nIf you can’t avoid it, enjoy it",
+  "천천히 가는 것을 두려워 말고, 중도에 그만두는 것을 두려워하라\nDo not fear going slowly, fear stopping midway",
 ];
